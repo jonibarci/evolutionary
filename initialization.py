0a754d687360fa52d512bb860328361bfa2f345b
Fatjon Barçi <barcijoni@gmail.com>
2023-10-17 12:18:28 +0200

    initialization added

--- initialization.py
+++ initialization.py
@@ -32,3 +32,46 @@ def nearest_neighbor_initialization(num_individuals, distance_matrix):
 
 # If you're considering Cluster-Based Initialization, it would require a clustering algorithm (like K-means) 
 # and then solving TSP for each cluster, potentially using a simpler method or heuristic.
+
+# 3. Greedy Initialization
+def greedy_initialization(num_individuals, distance_matrix):
+    def nearest_city(current_city, unvisited):
+        return min(unvisited, key=lambda city: distance_matrix[current_city][city])
+    
+    population = []
+    for _ in range(num_individuals):
+        start_city = random.choice(range(len(distance_matrix)))
+        unvisited = set(range(len(distance_matrix)))
+        unvisited.remove(start_city)
+        individual = [start_city]
+        
+        while unvisited:
+            next_city = nearest_city(individual[-1], unvisited)
+            individual.append(next_city)
+            unvisited.remove(next_city)
+            
+        population.append(individual)
+    
+    return population
+
+# 4. Random Greedy Initialization
+def random_greedy_initialization(num_individuals, distance_matrix):
+    def nearest_city(current_city, unvisited):
+        return min(unvisited, key=lambda city: distance_matrix[current_city][city])
+    
+    population = []
+    for _ in range(num_individuals):
+        start_city = random.choice(range(len(distance_matrix)))
+        unvisited = set(range(len(distance_matrix)))
+        unvisited.remove(start_city)
+        individual = [start_city]
+        
+        while unvisited:
+            next_city = nearest_city(individual[-1], unvisited)
+            individual.append(next_city)
+            unvisited.remove(next_city)
+            
+        population.append(individual)
+    
+    return population
+
