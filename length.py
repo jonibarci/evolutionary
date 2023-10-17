3f5c5998deb76bf3f486a12a899f67f4e762f670
Fatjon Barçi <barcijoni@gmail.com>
2023-10-17 11:37:57 +0200

    length method added

---
+++ length.py
@@ -0,0 +1,48 @@
+def tsp_length(matrix, permutation):
+    """
+    Calculate the length of the cycle for a given permutation in TSP.
+    
+    Args:
+    - matrix (list of list of ints): The nonsymmetric distance matrix.
+    - permutation (list of ints): The cyclic permutation (a permutation of vertices).
+    
+    Returns:
+    - int: The length of the cycle for the given permutation.
+    """
+    n = len(permutation)
+    total_distance = 0
+    
+    for i in range(n - 1):
+        total_distance += matrix[permutation[i]][permutation[i + 1]]
+    
+    # Adding the distance from the last city to the first one to complete the cycle.
+    total_distance += matrix[permutation[n - 1]][permutation[0]]
+    
+    return total_distance
+
+# For optimization, you'd typically use a library or framework like genetic algorithms or other optimization methods.
+# Here's a naive approach just to demonstrate how you'd use the tsp_length function.
+def solve_tsp(matrix):
+    """
+    Naive TSP solver. Returns the shortest path found after checking all permutations.
+    
+    Args:
+    - matrix (list of list of ints): The nonsymmetric distance matrix.
+    
+    Returns:
+    - tuple: The best permutation and its length.
+    """
+    import itertools
+    
+    n = len(matrix)
+    best_permutation = None
+    best_length = float('inf')
+    
+    # Check all possible permutations
+    for perm in itertools.permutations(range(n)):
+        current_length = tsp_length(matrix, perm)
+        if current_length < best_length:
+            best_length = current_length
+            best_permutation = perm
+    
+    return best_permutation, best_length
